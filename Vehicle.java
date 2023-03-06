a61661f43fa1b102c5e534c3b5a1d1a616606969
Sundaram Umarvaishya <sundaramvaishya@gmail.com>
2023-03-06 20:27:58 +0530

    Disel Engine class I have Added

--- Vehicle.java
+++ Vehicle.java
@@ -3,8 +3,9 @@ public class Vehicle{
   
     public static void main(String[] args) {
     PetrolEngine pe = new PetrolEngine(); 
-
+    DeselEngine de = new DeselEngine();
        System.out.println("hellow World!"); 
        pe.start(); 
+       de.start();
     }
 }
