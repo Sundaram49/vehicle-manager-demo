f6f7e4e09ed93abb2b3b0504055dc0644e3518e4
Sundaram Umarvaishya <sundaramvaishya@gmail.com>
2023-03-07 19:35:42 +0530

    Added stop method in Ee

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -3,4 +3,7 @@ public class ElectricEngine {
     public void start(){
         System.out.println("Electric engine start ..");
     }
+    public void stop(){
+        System.out.println("Electric Engine stoped ...");
+    }
 }
